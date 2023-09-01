6f61a69b442ae9c7c2fd213c7666d82f5382158b
Shaine Fisher <107189240+iamshainefisher@users.noreply.github.com>
2023-09-01 19:49:48 +0100

    Add files via upload
    missing js file

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+//little random changes
+// primary
+    "IPAddress": "192.168.0.26",
+    "IPPort": 2512,
+    "Password": "stageapp",
+// secondary
+    "BackupIPAddress": "10.0.0.26",
+    "BackupIPPort": 2512,
+    "BackupPassword": "stageapp",
+
+    "SplitLines": null,
+    "SplitLinesNum": 0,
+
+    "ClockLocale": "en-US",
+}
